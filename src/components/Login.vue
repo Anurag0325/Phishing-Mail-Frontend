d8bc6a2919d710b3e5cddaa579bc757f987d1807
Anurag Kumar <akanuragkumar4@gmail.com>
2024-10-25 16:00:40 +0530

    Login Page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -40,7 +40,7 @@ export default {
     methods: {
         async login() {
             try {
-                const response = await fetch('http://127.0.0.1:5000/login', {
+                const response = await fetch('https://phishing-mail-application.onrender.com/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
